06acee61df819be9c9f875462e351fd0a7af2a1d
krishakapur <87826555+krishakapur@users.noreply.github.com>
2023-01-26 16:51:23 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+Peter_pan_song="";
+Harry_potter_theme_song="";
+
+
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+
+function preload(){
+    Peter_pan_song = loadSound("music2.mp3");
+    Harry_potter_theme_song = loadSound("music.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
